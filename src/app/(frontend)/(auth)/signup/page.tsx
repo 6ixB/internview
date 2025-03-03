33b6ba729086caa2fee8aa8f21cf122a8800f8e5
6ixB <124480418+6ixB@users.noreply.github.com>
2025-03-03 18:18:14 +0700

    feat(auth): add signup page

---
+++ src/app/(frontend)/(auth)/signup/page.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next/types'
+
+
+import { SignupForm } from '@/components/Auth/SignupForm'
+import Link from 'next/link'
+
+export default function SignupPage() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <SignupForm />
+      </div>
+    </div>
+  )
+}
+
+export function generateMetadata(): Metadata {
+    return {
+      title: `Signup`,
+    }
+  }
+  
--- src/components/Auth/LoginForm/index.tsx
+++ src/components/Auth/LoginForm/index.tsx
@@ -3,6 +3,7 @@ import { Button } from '@/components/ui/button'
 import { Card, CardContent } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import Link from 'next/link'
 
 export function LoginForm({
   className,
@@ -41,7 +42,7 @@ export function LoginForm({
                 </div>
                 <Input id="password" type="password" required />
               </div>
-              <Button type="submit" className="w-full" disabled>
+              <Button type="submit" className="w-full">
                 Login
               </Button>
               <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
@@ -79,10 +80,10 @@ export function LoginForm({
                 </Button>
               </div>
               <div className="text-center text-sm">
-                Don&apos;t have an account?{" "}
-                <a href="#" className="underline underline-offset-4">
+                Don&apos;t have an account?&nbsp;
+                <Link href="/signup" className="underline underline-offset-4">
                   Sign up
-                </a>
+                </Link>
               </div>
             </div>
           </form>
@@ -96,7 +97,7 @@ export function LoginForm({
         </CardContent>
       </Card>
       <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>&nbsp;
         and <a href="#">Privacy Policy</a>.
       </div>
     </div>
